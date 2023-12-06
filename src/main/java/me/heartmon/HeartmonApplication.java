a55aef2ce49300c58d482b1ed0b6d8eeea9626d7
yurrrrri <yu_ri_son@naver.com>
2023-12-06 16:41:39 +0900

    feat: Init

---
+++ src/main/java/me/heartmon/HeartmonApplication.java
@@ -0,0 +1,13 @@
+package me.heartmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartmonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartmonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/heartmon/HeartmonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.heartmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartmonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
