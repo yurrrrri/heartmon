2659dbe730fd5cd28a864322bb32c77e1f9cc556
yurison <yu_ri_son@naver.com>
2023-12-11 23:11:07 +0900

    feat: basic methods of repositories

--- src/main/java/me/heartmon/domain/heartTarget/repository/HeartTargetRepository.java
+++ src/main/java/me/heartmon/domain/heartTarget/repository/HeartTargetRepository.java
@@ -1,7 +1,13 @@
 package me.heartmon.domain.heartTarget.repository;
 
 import me.heartmon.domain.heartTarget.entity.HeartTarget;
+import me.heartmon.domain.instaMember.entity.InstaMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HeartTargetRepository extends JpaRepository<HeartTarget, Long> {
+
+    List<HeartTarget> findByFromInstaMember(InstaMember instaMember);
+    List<HeartTarget> findByFromInstaMember_Id(Long fromInstaMemberId);
 }
--- src/main/java/me/heartmon/domain/instaMember/repository/InstaMemberRepository.java
+++ src/main/java/me/heartmon/domain/instaMember/repository/InstaMemberRepository.java
@@ -3,5 +3,9 @@ package me.heartmon.domain.instaMember.repository;
 import me.heartmon.domain.instaMember.entity.InstaMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InstaMemberRepository extends JpaRepository<InstaMember, Long> {
+
+    Optional<InstaMember> findByUsername(String username);
 }
